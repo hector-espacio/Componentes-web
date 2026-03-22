d426b00a0598b4be6670afbd6df3587052581d97
hector <117048971+hector-espacio@users.noreply.github.com>
2026-03-22 07:39:11 -0300

    Actualizar copiarCodigo.js

--- CopiarCodigo/copiarCodigo.js
+++ CopiarCodigo/copiarCodigo.js
@@ -1,1 +1,40 @@
 
+        function copyCode(buttonElement) {
+
+            // Seleccionamos el texto del bloque
+
+            const textToCopy = document.getElementById('code-text').innerText;
+
+    const mensaje = "Este es el código que se copiará";
+
+
+
+            // Copiamos al portapapeles (sin alertas)
+
+            navigator.clipboard.writeText(textToCopy).then(() => {
+
+                
+
+                // Cambiamos el texto del botón temporalmente
+
+                const originalText = "Copiar código";
+
+                buttonElement.innerText = "Copiado";
+
+                buttonElement.style.color = "#3fb950"; // Cambia a verde al copiar
+
+
+
+                // Volver al estado original después de 2 segundos
+
+                setTimeout(() => {
+
+                    buttonElement.innerText = originalText;
+
+                    buttonElement.style.color = "#8b949e";
+
+                }, 2000);
+
+            });
+
+        }
