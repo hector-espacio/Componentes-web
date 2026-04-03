f3943338154041528332ac619648ef02e3615ddf
hector <117048971+hector-espacio@users.noreply.github.com>
2026-04-03 14:50:06 -0300

    Actualizar CuerpoPosteo.jsx

---
+++ Mis proyectos en servidor react/ejercicio-1/src/CuerpoPosteo.jsx
@@ -0,0 +1,12 @@
+function CuerpoPosteo() {
+  return (
+    <main>
+      <p>
+        Este es el cuerpo de mi posteo. Aprendiendo a usar componentes 
+        con funciones tradicionales.
+      </p>
+    </main>
+  );
+}
+
+export default CuerpoPosteo;
