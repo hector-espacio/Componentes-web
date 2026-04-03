8c29e466eea55cd7454b294a497117d0e7d8ee8b
hector-espacio <hectorhmm01@gmail.com>
2026-04-03 13:29:56 -0300

    Primer cambio hecho desde el celular con Acode

--- Mis proyectos en servidor react/hola-mundo/src/App.jsx
+++ Mis proyectos en servidor react/hola-mundo/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
      
-     <h1>Hola mundito desde pc sin contraseña es oficial</h1>
+     <h1>Hola mundito desde pc sin contraseña es oficial yupi</h1>
     </>
   )
 }
