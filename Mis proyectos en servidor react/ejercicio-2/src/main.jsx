953e97fb3f2124da31e5b2e34e211ad2be744a22
hector <117048971+hector-espacio@users.noreply.github.com>
2026-04-03 19:53:07 -0300

    Actualizar main.jsx

--- Mis proyectos en servidor react/ejercicio-2/src/main.jsx
+++ Mis proyectos en servidor react/ejercicio-2/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Importás la lógica que armamos con el vector
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// 1. Buscamos el div vacío con id 'root' que está en el index.html
+const rootElement = document.getElementById('root');
+
+// 2. Creamos la "raíz" de React en ese lugar
+const root = ReactDOM.createRoot(rootElement);
+
+// 3. Ejecutamos la aplicación
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
