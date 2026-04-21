3e9678a7925ffc628fcea0aaf477b463c860bf77
hector <117048971+hector-espacio@users.noreply.github.com>
2026-04-21 18:26:14 +0000

    Actualizando archivos del proyecto

---
+++ Practicas Modularizacion/ejemplo03-WebDiscExportada/src/App.jsx
@@ -0,0 +1,16 @@
+import Saludo from './componentes/Saludo'; // 1. Importamos el subcomponente
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Ejercicio 2: Modularización</h1>
+      
+      {/* 2. Usamos el subcomponente como si fuera una etiqueta HTML */}
+      <Saludo />
+      <Saludo /> 
+      
+    </div>
+  );
+}
+
+export default App;
