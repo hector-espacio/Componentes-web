de68335c1a4bf75afb46dc56746d44ecb7d6ec0e
hector <117048971+hector-espacio@users.noreply.github.com>
2026-04-21 08:27:12 +0000

    Proyecto inicial de React con Vite y ejemplo de modularización

---
+++ Practicas Modularizacion/ejemplo02-UnSupComponente/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+    <h1>Hola mundo</h1>
+    <p>esto es contenido</p>
+    <p>esto es otro contenido 2</p>
+    </>
+  );
+}
+
+// Se usa export default para que este archivo entregue la función App.
+// Sin esto, el archivo main.jsx no podría "ver" el h1 que creamos.
+export default App;
