eb90f99f99887fa3794934a421df16d1e1ab53a1
hector <117048971+hector-espacio@users.noreply.github.com>
2026-05-07 20:12:16 -0300

    Crear clase01-SubComponente.jsx

---
+++ CursoReact/ClaseZoomUnicoArchivo/clase01-SubComponente.jsx
@@ -0,0 +1,37 @@
+src/Saludo.jsx
+
+export function Saludo() {
+  return (
+    <div>
+      <h2>Hola desde el Subcomponente</h2>
+    </div>
+  )
+}
+‐--‐----------------------------------‐--‐----------------------------------
+src/App.jsx
+
+import {Saludo} from './Saludo.jsx'
+
+function App(){
+return (
+<main>
+<h1>Aplicacion Principal</h1>
+<Saludo />
+</main>
+)
+}
+export default App
+
+‐--‐----------------------------------‐--‐----------------------------------
+src/main.jsx
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+ </StrictMode>,
+)
