73001203585f4da252edb735f9a93e60d763ad9d
hector <117048971+hector-espacio@users.noreply.github.com>
2026-05-09 20:34:03 -0300

    Crear clase04EjercicioOficialA.jsx

---
+++ CursoReact/ClaseZoomUnicoArchivo/clase04EjercicioOficialA.jsx
@@ -0,0 +1,43 @@
+// src/TarjetaProducto.jsx
+import { useState } from 'react'; // No olvides importar useState aquí
+
+export function TarjetaProducto({ imagen, nombre, precio }) { 
+  // Cada tarjeta crea su propio estado independiente al nacer
+  const [favorito, setFavorito] = useState("si");
+
+  const cambiarFavorito = () => {
+    if (favorito === "si") {
+      setFavorito("no");
+    } else {
+      setFavorito("si");
+    }
+  };
+
+  return (
+    <div style={{ border: "black solid 2px", padding: "30px", borderRadius: "10px" }}>
+      <img src={imagen} alt={nombre} />
+      <h3>{nombre}</h3>
+      <p>${precio}</p>
+      <p style={{ color: 'darkblue' }}>
+        Favorito: 
+        <button onClick={cambiarFavorito}>
+          {favorito}
+        </button>
+      </p>
+    </div>
+  );
+}
+// --------------------------
+
+// src/App.jsx
+function App() {
+  const datos = [ ... ];
+
+  return (
+    <div style={{ display: 'flex', gap: '20px' }}>
+      <TarjetaProducto {...datos[0]} />
+      <TarjetaProducto {...datos[1]} />
+      <TarjetaProducto {...datos[2]} />
+    </div>
+  );
+}
